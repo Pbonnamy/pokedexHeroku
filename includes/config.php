632085090a1481a8019504e04c2135562cee0f7e
Pierre <bonnamy.77@gmail.com>
2021-01-09 14:06:48 +0100

    add database link

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=pokedex', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+		$bdd = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_72998cb6f9ce79f', 'b00da32204c12c', '9ac6a23b', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 	}catch(Exception $e){
 		die('Erreur : ' . $e->getMessage());
 	}
